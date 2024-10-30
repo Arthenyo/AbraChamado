17b42c8d5e3dc0f5a09ee8a3b723d2a677dfc47b
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2024-10-30 16:50:48 -0300

    feat: projeto criado

---
+++ api/src/test/java/com/arthenyo/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthenyo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
