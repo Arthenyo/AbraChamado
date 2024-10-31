1931aecb6ab2e48558a0adb75a6ec69ccb8ad1dd
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2024-10-31 08:28:04 -0300

    feat: classe acesso

---
+++ api/src/main/java/com/arthenyo/api/entities/Acesso.java
@@ -0,0 +1,18 @@
+package com.arthenyo.api.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_acesso")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Acesso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+}
