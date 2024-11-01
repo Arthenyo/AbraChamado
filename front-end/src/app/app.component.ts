be16d4cbecf520897f2b24866e40493c12f35d13
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2024-11-01 10:49:16 -0300

    feat: estrutura inicial

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Componente01Component } from "./componente01/componente01.component";
-import { Componente02Component } from "./componente02/componente02.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Componente01Component, Componente02Component],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
