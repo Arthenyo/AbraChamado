61717d5c9ebee6cb6a80558690d70c78515a4b56
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2024-11-19 11:50:52 -0300

    feat: migagrando para o banco postgresSQL

--- api/src/main/java/com/arthenyo/api/repositories/ChamadoRepository.java
+++ api/src/main/java/com/arthenyo/api/repositories/ChamadoRepository.java
@@ -2,32 +2,33 @@ package com.arthenyo.api.repositories;
 
 import com.arthenyo.api.entities.Chamado;
 import com.arthenyo.api.entities.enums.StatusChamado;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.Instant;
-import java.time.LocalDate;
 import java.util.List;
 
-public interface ChamadoRepository extends JpaRepository<Chamado,Long> {
+public interface ChamadoRepository extends JpaRepository<Chamado, Long> {
 
     @Query("SELECT c.statusChamado, COUNT(c) " +
             "FROM Chamado c " +
-            "WHERE FORMATDATETIME(c.criacaoChamado, 'yyyy-MM-dd') = :data " +
+            "WHERE TO_CHAR(c.criacaoChamado, 'YYYY-MM-DD') = :data " +
             "GROUP BY c.statusChamado")
     List<Object[]> countChamadosGroupedByStatus(@Param("data") String data);
 
     @Query("SELECT COUNT(c) " +
             "FROM Chamado c " +
-            "WHERE FORMATDATETIME(c.criacaoChamado, 'yyyy-MM-dd') = :data")
+            "WHERE TO_CHAR(c.criacaoChamado, 'YYYY-MM-DD') = :data")
     Long countChamadosByData(@Param("data") String data);
 
     @Query("SELECT c FROM Chamado c ORDER BY c.criacaoChamado DESC")
     List<Chamado> findTopChamados(Pageable pageable);
+
     List<Chamado> findByTituloContainingIgnoreCase(String titulo);
+
     List<Chamado> findByStatusChamadoAndCriacaoChamadoBetween(StatusChamado status, Instant inicio, Instant fim);
+
     List<Chamado> findByCriacaoChamadoBetween(Instant inicio, Instant fim);
 }
